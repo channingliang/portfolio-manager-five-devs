26aea2b35d5b4f717572448d6533694bd43f4422
channingliang <ycliang.cn@outlook.com>
2025-07-24 09:56:10 +0800

    add basic vue framework

---
+++ fe/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {Button} from "@/components/ui/button/index.js";
+</script>
+
+<template>
+  <div>
+    <Button variant="outline" class="text-2xl font-black">
+      tailwind + shadcn
+    </Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
